c21ffb2dc3e2ceecc69982ab41ad9c0e0da84487
Hafsa Yaman <hafsa7yaman@gmail.com>
2025-04-15 17:50:13 -0500

    new

---
+++ databases.py
@@ -0,0 +1,30 @@
+import mysql.connector
+
+
+def get_connection():
+    connection = mysql.connector.connect(
+        host = 'localhost',
+        user = 'root',
+        password = '6403007.Ankara',
+        database  = 'users',
+    autocommit=True)
+    return connection
+
+def create__table(con):
+    sql = "CREATE DATABASE IF NOT EXISTS users"
+    cursor = con.cursor()
+    cursor.execute(sql)
+    sql = """
+    CREATE TABLE IF NOT EXISTS users.customers (
+        customer_id INT NOT NULL AUTO_INCREMENT,
+        customer_name VARCHAR(100),
+        email_address VARCHAR(255),
+        credit_limit DECIMAL(18,2),
+    PRIMARY KEY (customer_id),
+    UNIQUE INDEX `email_address_uk` (email_address ASC) VISIBLE)
+    """
+
+    cursor.execute(sql) 
+
+def add_customer(con, customer_name, email_address, credit_limit):
+    sql = "
