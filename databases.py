a801960dfc99e41c1ed7454adea806a91a6d20e9
Hafsa Yaman <hafsa7yaman@gmail.com>
2025-04-15 22:57:08 -0500

    commit

--- databases.py
+++ databases.py
@@ -7,7 +7,7 @@ def get_connection():
         user = 'root',
         password = '6403007.Ankara',
         database  = 'users',
-    autocommit=True)
+        autocommit=True)
     return connection
 
 def create__table(con):
@@ -23,8 +23,16 @@ def create__table(con):
     PRIMARY KEY (customer_id),
     UNIQUE INDEX `email_address_uk` (email_address ASC) VISIBLE)
     """
-
-    cursor.execute(sql) 
+    cursor.execute(sql)
 
 def add_customer(con, customer_name, email_address, credit_limit):
-    sql = "
+    sql = 'INSERT INTO users.customers (customer_name, email_address, credit_limit) VALUES (%s, %s, %s)'
+    data = [cqustomer_name, email_address, credit_limit]
+    cursor = con.cursor()
+    cursor.execute(sql, data)
+    customer_id = cursor.lastrowid
+
+    return customer_id
+
+def get_customer(con, customer_id):
+    sql = "SELECT * FROM users.customers WHERE customer_id = %s"
