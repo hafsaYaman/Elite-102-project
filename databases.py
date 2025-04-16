a99946b37edb20ca5948bd6cbf24b7825717cdb9
Hafsa Yaman <hafsa7yaman@gmail.com>
2025-04-15 23:08:52 -0500

    commit2

--- databases.py
+++ databases.py
@@ -10,7 +10,7 @@ def get_connection():
         autocommit=True)
     return connection
 
-def create__table(con):
+def create_table(con):
     sql = "CREATE DATABASE IF NOT EXISTS users"
     cursor = con.cursor()
     cursor.execute(sql)
@@ -27,7 +27,7 @@ def create__table(con):
 
 def add_customer(con, customer_name, email_address, credit_limit):
     sql = 'INSERT INTO users.customers (customer_name, email_address, credit_limit) VALUES (%s, %s, %s)'
-    data = [cqustomer_name, email_address, credit_limit]
+    data = [customer_name, email_address, credit_limit]
     cursor = con.cursor()
     cursor.execute(sql, data)
     customer_id = cursor.lastrowid
@@ -36,3 +36,43 @@ def add_customer(con, customer_name, email_address, credit_limit):
 
 def get_customer(con, customer_id):
     sql = "SELECT * FROM users.customers WHERE customer_id = %s"
+    data = [customer_id]
+    cursor = con.cursor()
+    cursor.execute(sql, data)
+    result = cursor.fetchone()
+
+    return result
+
+def get_customer_by_email(con, email_address):
+    sql = "SELECT * FROM demo.customers WHERE email_address = %s"
+    data = [email_address]
+    cursor = con.cursor()
+    cursor.execute(sql, data)
+    result = cursor.fetchone()
+
+    return result
+
+def main():
+    con = get_connection()
+    print("connection open")
+    create_table(con)
+    print("table created")
+    customer_id = add_customer(con, 'person lastname', '222@demo.com', 400)
+    print(f"Inserted user {customer_id}")
+    customer_id = add_customer(con, 'hehe notaName', '768@demo.com', 12000)
+    print(f"Inserted customer {customer_id}")
+    
+    customer_id = add_customer(con, 'Business name', '999@demo.com', 2500)
+    print(f"Inserted customer {customer_id}")
+    customer = get_customer(con, customer_id)
+    print(customer)
+
+    customer = get_customer_by_email(con, '789@demo.com')
+    print(customer)
+
+
+    con.close()
+
+
+if (__name__ == '__main__'):
+    main()
